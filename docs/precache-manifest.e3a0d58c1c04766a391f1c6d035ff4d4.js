3188874e36865bdd2c9a733167b95e29d1873463
marinadelafuente <mdelafuente18@gmail.com>
2019-12-19 16:51:39 +0100

    create docs

---
+++ docs/precache-manifest.e3a0d58c1c04766a391f1c6d035ff4d4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e03d6b9900778038cd569ecfbc6aba8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4d8e059465fa3cdd140b",
+    "url": "./static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "da0e58351d6c4fd3bd70",
+    "url": "./static/js/2.32289a3b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "./static/js/2.32289a3b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4d8e059465fa3cdd140b",
+    "url": "./static/js/main.e49aee62.chunk.js"
+  },
+  {
+    "revision": "3beb9af01f9c41ddc497",
+    "url": "./static/js/runtime-main.ac8a9498.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
