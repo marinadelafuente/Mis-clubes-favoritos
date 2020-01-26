70e54beba7d6a34f0e123eba1de333e7a72902d2
marinadelafuente <mdelafuente18@gmail.com>
2020-01-26 18:01:13 +0100

    add css styling

--- src/components/Club.js
+++ src/components/Club.js
@@ -1,14 +1,17 @@
 import React from 'react';
+import '../stylesheets/Club.css';
 
 function Club(props) {
     console.log(props);
     console.log(props.club.members);
 
     return (
-        <li className="border--medium">
-            <i className={props.club.fa}></i>
-            <h2>{props.club.name}</h2>
-            <p>Members</p>
+        <li className="border--medium list__item">
+            <div className="club__title">
+                <i className={props.club.fa}></i>
+                <h2 className="subtitle">{props.club.name}</h2>
+            </div>
+            <p className="members__title">Members</p>
             <ul className="club__members">
                 {props.club.members.map((member, i) => {
                     return (
--- src/components/ClubList.js
+++ src/components/ClubList.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Club from './Club';
+import '../stylesheets/ClubList.css';
 
 function ClubList(props) {
     console.log(props.clubs)
     return (
         <div>
+            <h1 className="title">Club List</h1>
             <ul className="list">
                 {props.clubs.map((club, i) => {
                     return <Club
